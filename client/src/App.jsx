e30f166e6cb4d08d0bffde7cf974a67104268367
duttaanirban <duttaanirbanad@gmail.com>
2025-08-06 12:06:08 +0530

    styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import Layout from "./pages/Layout.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
+import Blogtitles from "./pages/Blogtitles.jsx"
+import Writearticles from "./pages/Writearticles.jsx"
 
 function App() {
   
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline bg-amber-100 text-center p-4">
-    Hello world!
-  </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="blogtitles" element={<Blogtitles />} />
+          <Route path="writearticles" element={<Writearticles />} />
+
+        </Route>
+      </Routes>
     </>
   )
 }
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+        <Outlet />
+    </div>
   )
 }
 
