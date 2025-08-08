0fa346bb50fb5d0705fad34b7811bfd8f09dc328
duttaanirban <duttaanirbanad@gmail.com>
2025-08-08 22:30:04 +0530

    sidebar

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sidebar = ({sidebar, setSidebar}) => {
+  return (
+    <div className={`w-60 bg-white border-r border-gray-200 flex
+    flex-col justify-between items-center max-sm:absolute top-14
+    bottom-0 ${sidebar ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300
+    ease-in-out`}>
+        <div className='my-7 w-full'>
+            <img />
+        </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,8 +1,22 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
+import { assets } from "../assets/assets";
+import { useState } from "react";
+import { X } from "lucide-react";
+import { Menu } from "lucide-react";
 
 const Layout = () => {
+
+  const navigate = useNavigate();
+  const [sidebar, setSidebar] = useState(false);
   return (
-    <div>
+    <div className="flex flex-col items-start justify-start h-screen">
+        <nav>
+          <img src={assets.logo} alt="" onClick={() => navigate('/')} />
+          {
+            sidebar ? <X onClick={() => setSidebar(false)} className="w-6 h-6 text-gray-600 sm:hidden"/>
+            : <Menu onClick={() => setSidebar(true)} className="w-6 h-6 text-gray-600 sm:hidden"/>
+          }
+        </nav>
         <Outlet />
     </div>
   )
