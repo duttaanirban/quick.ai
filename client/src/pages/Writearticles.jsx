e1776dc2586da08c07405e923f7145b8f7d6a94d
duttaanirban <duttaanirbanad@gmail.com>
2025-08-11 20:48:39 +0530

    write Article updated

--- client/src/pages/Writearticles.jsx
+++ client/src/pages/Writearticles.jsx
@@ -1,4 +1,4 @@
-import { Sparkles } from 'lucide-react'
+import { Edit, Sparkles } from 'lucide-react'
 import { useState } from 'react'
 
 
@@ -16,11 +16,15 @@ const Writearticles = () => {
   const [selectedLength, setSelectedLength] = useState(articleLength[0])
   const [input, setInput] = useState('')
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4
     text-slate-700">
       {/* left col */}
-      <form className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+      <form onSubmit={handleSubmit} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
         <div className='flex items-center gap-3'>
           <Sparkles className='w-6 text-[#4A7AFF]'/>
           <h1 className='text-xl font-semibold'>Article Configuration</h1>
@@ -38,11 +42,24 @@ const Writearticles = () => {
           key={index} >{item.text}</span>
           ))}
         </div>
+        <br/>
+        <button className='w-full flex justify-center items-center gap-2
+        bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6
+        text-sm rounded-lg cursor-pointer'>
+          <Edit className='w-5'/>
+          Generate Article
+        </button>
       
       
       </form>
 
       {/* right col*/}
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+          <div className='flex items-center gap-3'>
+            <Edit className='w-5 h-5 text-[#4A7AFF]'/>
+            <h1 className='text-xl font-semibold'>Article Configuration</h1>
+          </div>
+      </div>
 
     </div>
   )
