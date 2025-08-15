14392d8c88630218ae725d38c1f57cf17854ad63
duttaanirban <duttaanirbanad@gmail.com>
2025-08-15 20:09:20 +0530

    neon and clerk setup for backend done

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import e from "express";
 import cors from "cors";
 import 'dotenv/config'
+import {clerkMiddleware, requireAuth} from '@clerk/express'
 
 
 const app = e();
 
 app.use(cors());
 app.use(e.json());
+app.use(clerkMiddleware());
+
 app.get('/', (req, res)=>{
     res.send('server is live');
 })
+app.use(requireAuth())
 
 const PORT = process.env.PORT || 3000;
 
