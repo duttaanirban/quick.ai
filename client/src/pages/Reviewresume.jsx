63f19eb741b3834eef4347ed6dd5816007f3a61b
duttaanirban <duttaanirbanad@gmail.com>
2025-08-18 20:33:59 +0530

    connected review resume

--- client/src/pages/Reviewresume.jsx
+++ client/src/pages/Reviewresume.jsx
@@ -1,12 +1,46 @@
-import { Eraser, FileText, Sparkles } from "lucide-react"
+import { FileText, Sparkles } from "lucide-react"
+import { use } from "react"
 import { useState } from "react"
+import axios from "axios"
+import { useAuth } from "@clerk/clerk-react"
+import toast from "react-hot-toast"
+import Markdown from "react-markdown"
 
 
 const Reviewresume = () => {
+
+
   const [input, setInput] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState('')
+
+  const {getToken} = useAuth(); 
     
     const handleSubmit = async (e) => {
-      e.preventDefault()
+      e.preventDefault();
+      try {
+        setLoading(true);
+      
+        const formData = new FormData();
+        formData.append('resume', input);
+        const { data } = await axios.post('/api/ai/review-resume', formData, {
+          headers: {
+            Authorization: `Bearer ${await getToken()}`
+          }
+        });
+        if (data.success) {
+          setContent(data.content);
+        } else {
+          toast.error(data.message || 'Resume review failed');
+        }
+      }
+      catch (error) {
+        console.error('Error reviewing resume:', error);
+        toast.error(error?.response?.data?.error || error.message || 'Resume review failed');
+      }
+      finally {
+        setLoading(false);
+      }
     }
 
   return (
@@ -25,10 +59,13 @@ const Reviewresume = () => {
         </input>
 
         <p className='text-xs text-gray-500 font-light mt-1'>supports .pdf, .docx and other document formats</p>
-        <button className='w-full flex justify-center items-center gap-2
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2
         bg-gradient-to-r from-[#107201] to-[#00DA83] text-white px-4 py-2 mt-6
         text-sm rounded-lg cursor-pointer'>
-          <FileText className='w-5'/>
+          {
+            loading ? <span className='w-4 h-4 my-1 rounded-full border-2
+            border-t-transparent animate-spin'></span> : <FileText className='w-5'/>
+          }
           Generate Report
         </button>
       
@@ -41,12 +78,23 @@ const Reviewresume = () => {
             <FileText className='w-5 h-5 text-[#00DA83]'/>
             <h1 className='text-xl font-semibold'>Processed Resume</h1>
           </div>
-          <div className='flex-1 flex justify-center items-center'>
+          {
+            !content ? (
+               <div className='flex-1 flex justify-center items-center'>
             <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
               <FileText className='w-9 h-9' />
               <p>Upload resume and click "Generate Report" to get started</p>
             </div>
           </div>
+            ) : (
+              <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+                <div className="reset-tw">
+                  <Markdown>{content}</Markdown>
+                </div>
+              </div>
+            )
+          }
+         
       </div>
     </div>
   )
